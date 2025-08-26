984c157d2f6912540b81c706d0493ed80e67b8ff
Mohammad Furquan Jamadar <mmmj131310@gmail.com>
2025-08-26 12:08:15 +0530

    First commit

---
+++ WEBAPI/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,100 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.Xml;
+
+namespace CodePulse.API.Controllers
+{
+    //https://localhost:xxxx/api/Categories
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory([FromBody]CreateCategoryRequestDto request)
+        {
+            // Map DTO to Domain Model
+
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await categoryRepository.CreateAsync(category);
+
+            // Domain model to DTO
+
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+
+            };
+
+            return Ok(response);
+        }
+
+
+        // GET METHOD: https://localhost:7023/api/Categories
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetAllCategories()
+        {
+            var categories = await categoryRepository.GetAllAsync();
+
+            //Map Domain Model to DTO
+
+            var response = new List<CategoryDto>();
+            foreach (var category in categories)
+            {
+                response.Add(new CategoryDto
+                {
+                    Id = category.Id,
+                    Name = category.Name,
+                    UrlHandle = category.UrlHandle
+                });
+
+            }
+
+            return Ok(response);
+        }
+
+        // GET by id: https://localhost:7023/api/Categories/{id}
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+
+        public async Task<IActionResult> GetCategoryById([FromRoute]Guid id)
+        {
+          var existingCategory =  await categoryRepository.GetById(id);
+
+          if (existingCategory is null)
+            {
+                return NotFound();
+            }
+
+            var response = new CategoryDto
+            {
+                Id = existingCategory.Id,
+                Name = existingCategory.Name,
+                UrlHandle = existingCategory.UrlHandle
+            };
+
+            return Ok(response);
+
+        }
+    }
+}
---
+++ WEBAPI/CodePulse.API/CodePulse.API/Repositories/Implementation/CategoryRepository.cs
@@ -0,0 +1,36 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePulse.API.Repositories.Implementation
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDBContext dBContext;
+
+        public CategoryRepository(ApplicationDBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+        public async Task<Category> CreateAsync(Category category)
+        {
+            await dBContext.Categories.AddAsync(category);
+            await dBContext.SaveChangesAsync();
+
+            return category;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+          return  await dBContext.Categories.ToListAsync();
+        }
+
+        public async Task<Category?> GetById(Guid id)
+        {
+           return await dBContext.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            
+            
+        }
+    }
+}
